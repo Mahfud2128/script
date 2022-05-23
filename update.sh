868610f255fdd385639316954af81117178a2873
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-05-24 01:30:32 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ apt upgrade && apt replace -y
 echo -e "${green}START UPDATE${NC}"
 sleep 5
 
-wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/V1/kemaskini.sh && chmod +x kemaskini.sh && ./kemaskini.sh
+wget https://raw.githubusercontent.com/GH-reyz/script/main/kemaskini.sh && chmod +x kemaskini.sh && ./kemaskini.sh
 
 echo -e "${green}UPDATE SELESAI${NC}"
 echo " Reboot 5 Sec"
